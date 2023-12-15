e0191aa996bdfd8845ef9277585159efdee86013
Piort Łopatecki <piotr.lopatecki@gmail.com>
2023-12-15 20:59:12 +0100

    poprawki

--- doniczka_backend_app/views.py
+++ doniczka_backend_app/views.py
@@ -4,11 +4,14 @@ from .models import Plant
 from .serializers import PlantModelSerializer,PlantCreatingSerializer, TemperatureandHumiditySerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
+import Adafruit_DHT
 
 class PlantViewSet(GenericViewSet):
     permission_classes = [AllowAny]
     queryset = Plant.objects.all()
     serializer_class = PlantModelSerializer
+    sensor = Adafruit_DHT.DHT11
+    pin = 4
 
     @action(detail=False, methods=['post'])
     def create_plant(self,request):
@@ -17,13 +20,18 @@ class PlantViewSet(GenericViewSet):
         serialzier.save()
         return Response('Roślina zasadzona ;D')
 
-    @action(detail=False, methods=['put'])
-    def update_temp(self, request):
-        serializer = TemperatureandHumiditySerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response('Temperatura zaktualizowana')
+    # @action(detail=False, methods=['put'])
+    # def update_temp(self, request):
+    #     humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.pin)
+    #     serializer = TemperatureandHumiditySerializer(data={'temperature':temperature, 'air_humidity':humidity})
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response('Temperatura zaktualizowana')
     
     @action(detail=False, methods=['get'])
     def get_info(self,request):
+        humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.pin)
+        serializer = TemperatureandHumiditySerializer(data={'temperature':temperature, 'air_humidity':humidity})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(self.serializer_class(Plant.objects.all()).data)
