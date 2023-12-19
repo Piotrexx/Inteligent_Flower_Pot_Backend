5fd83bfb7330b2d33b5810882a519ff27ea68e6b
Piort Łopatecki <piotr.lopatecki@gmail.com>
2023-12-19 21:37:44 +0100

    threading

---
+++ raspcode/read_hum_and_water.py
@@ -0,0 +1,24 @@
+from adafruit_seesaw.seesaw import Seesaw
+from time import sleep
+import board
+from gpiozero import LED
+
+def check():
+    pump = LED(18)
+
+    i2c_bus = board.I2C()
+
+    ss = Seesaw(i2c_bus, addr=0x36)
+
+    while True:
+
+        hum = ss.moisture_read()
+
+
+        temp = ss.get_temp()
+        if int(hum) > 600:
+            pump.on()
+            pump.off()
+            sleep(15)
+        print("temp: " + str(temp) + "  moisture: " + str(hum))
+        sleep(1)
