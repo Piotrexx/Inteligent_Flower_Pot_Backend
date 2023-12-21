c22b415a37801b82782fe3fcbcf9709a7046676c
Piort Łopatecki <piotr.lopatecki@gmail.com>
2023-12-21 19:27:33 +0100

    cron django

--- doniczka_backend_app/cron.py
+++ doniczka_backend_app/cron.py
@@ -22,6 +22,10 @@ def water_plants():
         pump.on()
         pump.off()
         sleep(15)
+    pump = LED(18)
+    pump.on()
+    pump.off()
+    sleep(15)
     print("temp: " + str(temp) + "  moisture: " + str(hum))
 
 
