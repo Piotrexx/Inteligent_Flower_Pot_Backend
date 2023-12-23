aab5842a2bf0cf8b5e943f7c28be1de0c023866c
Piort Łopatecki <piotr.lopatecki@gmail.com>
2023-12-23 22:03:24 +0100

    test

--- raspcode/read_hum_and_water.py
+++ raspcode/read_hum_and_water.py
@@ -2,27 +2,30 @@ from adafruit_seesaw.seesaw import Seesaw
 from time import sleep
 import board
 from gpiozero import LED
+import Adafruit_DHT
+from doniczka_backend_app.serializers import GroundHumiditySerializer
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from doniczka_backend_app.models import Plant
 
 def check():
     
-
     i2c_bus = board.I2C()
-
+    
     ss = Seesaw(i2c_bus, addr=0x36)
 
-    while True:
-
-        hum = ss.moisture_read()
-
-
-        temp = ss.get_temp()
-        if int(hum) < 600:
-            pump = LED(18)
-            print('test')
-            pump.on()
-            pump.off()
-            sleep(15)
-        print("temp: " + str(temp) + "  moisture: " + str(hum))
-        sleep(1)
+    hum = ss.moisture_read()
+    temp = ss.get_temp()
+    serializer = GroundHumiditySerializer(instance=Plant.objects.get(id=1), data={'ground_humidity':hum})
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    
+    if int(hum) < 400:
+        pump = LED(18)
+        print('test')
+        pump.on()
+        pump.off()
+        sleep(15)        
+    print("temp: " + str(temp) + "  moisture: " + str(hum))
 
 check()
