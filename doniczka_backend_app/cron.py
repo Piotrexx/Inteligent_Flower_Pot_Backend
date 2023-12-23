8e8c995cb0106c52860c057f89161ad60be6fec4
Piort Łopatecki <piotr.lopatecki@gmail.com>
2023-12-23 20:49:31 +0100

    test

--- doniczka_backend_app/cron.py
+++ doniczka_backend_app/cron.py
@@ -15,15 +15,21 @@ def water_plants():
 
     hum = ss.moisture_read()
     temp = ss.get_temp()
-    serializer = GroundHumiditySerializer(instance=Plant.objects.get(id=1), data={'ground_humidity':hum})
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-    if int(hum) < 400:
-        pump = LED(18)
-        print('test')
-        pump.on()
-        pump.off()
-        sleep(15)
+    try:
+        get_object_or_404(Plant.objects.get(id=1))
+        serializer = GroundHumiditySerializer(instance=Plant.objects.get(id=1), data={'ground_humidity':hum})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        
+        if int(hum) < 400:
+            pump = LED(18)
+            print('test')
+            pump.on()
+            pump.off()
+            sleep(15)
+    except:
+        print('Trzeba zrobić roślinę')
+
         
     print("temp: " + str(temp) + "  moisture: " + str(hum))
 
