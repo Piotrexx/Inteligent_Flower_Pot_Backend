176310597ac62423df3cfd5ca28b4d4631e94d76
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-01-06 14:06:55 +0100

    time fix

--- doniczka_backend_app/models.py
+++ doniczka_backend_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator 
-
+from time import timezone
 
 class Plant(models.Model):
 
@@ -15,4 +15,4 @@ class Plant(models.Model):
     air_humidity = models.IntegerField(default=0)
     water_level = models.BooleanField(default=False)
     temperature = models.FloatField(default=0)
-    last_watering = models.DateTimeField()
+    last_watering = models.DateTimeField(default=timezone.now)
