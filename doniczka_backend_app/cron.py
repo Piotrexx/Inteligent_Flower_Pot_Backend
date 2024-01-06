08cd3e960b63f4539e074ca21a655bedcdc8884a
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-01-06 13:29:30 +0100

    test

--- doniczka_backend_app/cron.py
+++ doniczka_backend_app/cron.py
@@ -3,11 +3,8 @@ from time import sleep
 import board
 from gpiozero import LED
 import Adafruit_DHT
-# from .serializers import GroundHumiditySerializer
+from .serializers import GroundHumiditySerializer
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-# from .models import Plant
-
 def water_plants():
     i2c_bus = board.I2C()
     
@@ -15,24 +12,6 @@ def water_plants():
 
     hum = ss.moisture_read()
     temp = ss.get_temp()
-    # try:
-    #     get_object_or_404(Plant.objects.get(id=1))
-    #     serializer = GroundHumiditySerializer(instance=Plant.objects.get(id=1), data={'ground_humidity':hum})
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-        
-    #     if int(hum) < 400:
-    #         pump = LED(18)
-    #         print('test')
-    #         pump.on()
-    #         pump.off()
-    #         sleep(15)
-    # except:
-    #     print('Trzeba zrobić roślinę')
-
-    # serializer = GroundHumiditySerializer(instance=Plant.objects.get(id=1), data={'ground_humidity':hum})
-    # serializer.is_valid(raise_exception=True)
-    # serializer.save()
     
     if int(hum) < 400:
         pump = LED(18)
--- doniczka_backend_app/models.py
+++ doniczka_backend_app/models.py
@@ -3,8 +3,14 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Plant(models.Model):
+
+    PLANT_CHOICES = {
+        "Kaktus": "Kaktus",
+        "Kawa": "Kawa"
+    }
+
     plant_name = models.CharField(max_length=255)
-    plant_specie = models.CharField(max_length=100)
+    plant_specie = models.CharField(max_length=10, choices=PLANT_CHOICES)
     ground_humidity = models.IntegerField(default=0,validators=[MinValueValidator(1), MaxValueValidator(100)])
     air_humidity = models.IntegerField(default=0,validators=[MinValueValidator(1), MaxValueValidator(100)])
     water_level = models.BooleanField(default=False)
