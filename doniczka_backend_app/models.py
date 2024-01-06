a1a9d240616f4fd2c16a58592e634ac79ebd6eb5
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-01-06 13:59:40 +0100

    update cronjobs and model

--- doniczka_backend_app/models.py
+++ doniczka_backend_app/models.py
@@ -4,10 +4,10 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Plant(models.Model):
 
-    PLANT_CHOICES = {
-        "Kaktus": "Kaktus",
-        "Kawa": "Kawa"
-    }
+    PLANT_CHOICES = [
+        ("Kaktus", "Kaktus"),
+        ("Kawa", "Kawa")
+    ]
 
     plant_name = models.CharField(max_length=255)
     plant_specie = models.CharField(max_length=10, choices=PLANT_CHOICES)
