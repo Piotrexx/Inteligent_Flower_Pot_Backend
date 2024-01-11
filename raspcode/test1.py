0c21b6fc4e573049739038e340cc642296debee9
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-01-11 21:28:33 +0100

    test

--- raspcode/test1.py
+++ raspcode/test1.py
@@ -2,10 +2,11 @@ from adafruit_seesaw.seesaw import Seesaw
 from time import sleep
 import board
 from gpiozero import LED
+pump = LED(18)
 
 while True:
     sleep(5)
-    pump = LED(18)
+    
     pump.on()
     pump.off()
     sleep(5)
