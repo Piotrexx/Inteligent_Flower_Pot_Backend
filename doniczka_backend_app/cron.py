63700c2a2b4b7cf97907f892bb0cf08da3e5eec7
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-01-28 18:55:40 +0100

    dead pompa

--- doniczka_backend_app/cron.py
+++ doniczka_backend_app/cron.py
@@ -14,43 +14,43 @@ def use_pump_and_save(base_url, now):
     pump = LED(18)
     pump.on()
     pump.off()
-    sleep(15)
+    sleep(10)
     requests.put(url=base_url+'update_date/', data={'last_watering':now})        
 
 
 def water_plants():
-    i2c_bus = board.I2C()
+#     i2c_bus = board.I2C()
     
-    ss = Seesaw(i2c_bus, addr=0x36)
-    base_url = 'http://127.0.0.1:8000/plants/'
-    plant_type = requests.get(url=base_url+'get_type/').json()
-    print(plant_type)
-    last_watering = requests.get(url=base_url+'get_date/').json()
-    print(type(last_watering['last_watering']))
-    now = datetime.now()
-    print(now)
-    print(type(now))
-    hum = ss.moisture_read()
-    temp = ss.get_temp()
-    update_ground = requests.put(url=base_url+'update_ground/', data={'ground_humidity': hum})
+#     ss = Seesaw(i2c_bus, addr=0x36)
+#     base_url = 'http://127.0.0.1:8000/plants/'
+#     plant_type = requests.get(url=base_url+'get_type/').json()
+#     print(plant_type)
+#     last_watering = requests.get(url=base_url+'get_date/').json()
+#     print(type(last_watering['last_watering']))
+#     now = datetime.now()
+#     print(now)
+#     print(type(now))
+#     hum = ss.moisture_read()
+#     temp = ss.get_temp()
+#     update_ground = requests.put(url=base_url+'update_ground/', data={'ground_humidity': hum})
     
-    if str(plant_type['plant_specie']).lower() == 'kawa':
+#     if str(plant_type['plant_specie']).lower() == 'kawa':
     
-        if int(hum) < 400:
-            use_pump_and_save(base_url=base_url, now=now)
+#         if int(hum) < 400:
+#             use_pump_and_save(base_url=base_url, now=now)
     
-    if plant_type['plant_specie'].lower() == 'kaktus':
-        if int(hum) < 400:
-            if last_watering['last_watering'] != 0:
-                if int(last_watering['last_watering'][8:10]) < int(now.strftime("%d/%m/%Y %H:%M:%S")[3:5]):
-                    use_pump_and_save(base_url=base_url, now=now)
-            else:
-                use_pump_and_save(base_url=base_url, now=now)
+#     if plant_type['plant_specie'].lower() == 'kaktus':
+#         if int(hum) < 400:
+#             if last_watering['last_watering'] != 0:
+#                 if int(last_watering['last_watering'][8:10]) < int(now.strftime("%d/%m/%Y %H:%M:%S")[3:5]):
+#                     use_pump_and_save(base_url=base_url, now=now)
+#             else:
+#                 use_pump_and_save(base_url=base_url, now=now)
 
 
-if __name__ == '__main__':
-    water_plants()
-
+# if __name__ == '__main__':
+#     water_plants()
+    pass
 
 
 
